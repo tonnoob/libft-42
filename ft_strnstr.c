cb472b84ab69292770a1f96f4a59466ca9fb1839
tonnoob <ottonsouzanto@gmail.com>
2025-07-28 01:23:18 -0300

    entendendo melhor comparação de byte

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,4 +10,26 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stddef.h>
 
+char	*strnstr(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (s2[0] == '\0')
+		return (s1);
+	while (i < n)
+	{
+		j = 0;
+		while (i + j < n && s1[i + j] == s2[j])
+		{
+			if (s2[j + 1] == '\0')
+				return ((char *)&s1[i]);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
